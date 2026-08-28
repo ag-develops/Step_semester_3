db30c200784ab24a6ab7306b3347dc57ed77f013
Ayanjit Ganguly <gangulyayanjit@gmail.com>
2026-08-29 00:01:28 +0530

    Add static employee example

---
+++ M5.java
@@ -0,0 +1,31 @@
+class Employee {
+
+    String empName;
+    double salary;
+
+    static String companyName = "Bright Horizon Technologies";
+    static int employeeCount = 0;
+
+    Employee(String empName, double salary) {
+        this.empName = empName;
+        this.salary = salary;
+
+        employeeCount++;
+    }
+
+    static void printCompanyInfo() {
+        System.out.println(companyName);
+        System.out.println("Employees on record: " + employeeCount);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Employee e1 = new Employee("Ayan", 50000);
+        Employee e2 = new Employee("Rahul", 60000);
+        Employee e3 = new Employee("Priya", 55000);
+
+        Employee.printCompanyInfo();
+    }
+}
